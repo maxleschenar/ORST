7eca165f27ae035cae5e3edc7974828c9eb0e877
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-09-26 13:27:57 +0200

    Add an script for tracking if user is grabbing an object
    This is used to prevent interacting with the UI or other elements while
    the user is grabbing an object.

---
+++ ORST/Assets/Scripts/Runtime/Interactions/HandGrabTracker.cs
@@ -0,0 +1,16 @@
+﻿using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+namespace Runtime.Interactions {
+    /// <summary>
+    /// An <see cref="IActiveState"/> implementation that is active when the
+    /// provided <see cref="IHandGrabState"/> is grabbing an object.
+    /// </summary>
+    public class HandGrabTracker : SerializedMonoBehaviour, IActiveState {
+        [OdinSerialize, Required] private IHandGrabState m_HandGrabState;
+
+        public bool Active => m_HandGrabState.IsGrabbing;
+    }
+}
