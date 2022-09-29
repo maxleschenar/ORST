b7661c8028445b710df6fd6b549d8abae097ff32
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-09-29 15:19:56 +0200

    Add MonoSingleton and AutoMonoSingleton scripts

---
+++ ORST/Assets/Scripts/Foundation/Singleton/MonoSingleton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace ORST.Foundation.Singleton {
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T> {
+        private static T s_Instance;
+
+        public static T Instance {
+            get {
+                if (s_Instance == null) {
+                    s_Instance = FindObjectOfType<T>();
+                }
+
+                if (s_Instance == null) {
+                    Debug.LogError($"No instance of {typeof(T).Name} found");
+                }
+
+                return s_Instance;
+            }
+        }
+
+        private void Awake() {
+            if (s_Instance != null && s_Instance != this) {
+                Debug.LogError($"Multiple instances of {typeof(T).Name} found");
+                Destroy(this);
+                OnAwake();
+                return;
+            }
+
+            s_Instance = this as T;
+            OnAwake();
+        }
+
+        protected virtual void OnAwake() { }
+    }
+}
