d22a1993e4c01a1c75dbef39d5682fe0784ce43e
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-10-06 15:28:28 +0200

    Add IsTeleporting IActiveState

---
+++ ORST/Assets/Scripts/Core/Interactions/IsTeleporting.cs
@@ -0,0 +1,12 @@
+﻿using Oculus.Interaction;
+using ORST.Core.Movement;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ORST.Core.Interactions {
+    public class IsTeleporting : MonoBehaviour, IActiveState {
+        [SerializeField, Required] private TeleportInputHandlerHands m_TeleportInputHandler;
+
+        public bool Active => m_TeleportInputHandler.GetIntention() != LocomotionTeleport.TeleportIntentions.None;
+    }
+}
