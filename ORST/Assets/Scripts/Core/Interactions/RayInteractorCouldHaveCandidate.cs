9ea1de0429d4a7391359d0cd3e1389705aa20fea
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-10-06 12:08:49 +0200

    Add IActiveState that tracks whether the ray is pointing at a canvas

---
+++ ORST/Assets/Scripts/Core/Interactions/RayInteractorCouldHaveCandidate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Oculus.Interaction;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ORST.Core.Interactions {
+    public class RayInteractorCouldHaveCandidate : MonoBehaviour, IActiveState {
+        [SerializeField, Required] private RayInteractor m_Interactor;
+
+        [ShowInInspector] public bool Active => CouldHaveCandidate();
+
+        private bool CouldHaveCandidate() {
+            if (m_Interactor == null) {
+                return false;
+            }
+
+            if (m_Interactor.HasCandidate) {
+                return true;
+            }
+
+            IEnumerable<RayInteractable> interactables = RayInteractable.Registry.List(m_Interactor);
+            foreach (RayInteractable interactable in interactables) {
+                if (!interactable.Raycast(m_Interactor.Ray, out _, m_Interactor.MaxRayLength, false)) {
+                    continue;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
