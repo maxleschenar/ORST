99be4eef07329651aede0a9e5ee9f24c1ac9dbea
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-10-06 15:33:11 +0200

    Rename HandGrabTracker to IsGrabbing

--- ORST/Assets/Scripts/Core/Interactions/ActiveStates/IsGrabbing.cs
+++ ORST/Assets/Scripts/Core/Interactions/ActiveStates/IsGrabbing.cs
@@ -8,7 +8,7 @@ namespace ORST.Core.Interactions {
     /// An <see cref="IActiveState"/> implementation that is active when the
     /// provided <see cref="IHandGrabState"/> is grabbing an object.
     /// </summary>
-    public class HandGrabTracker : SerializedMonoBehaviour, IActiveState {
+    public class IsGrabbing : SerializedMonoBehaviour, IActiveState {
         [OdinSerialize, Required] private IHandGrabState m_HandGrabState;
 
         public bool Active => m_HandGrabState.IsGrabbing;
