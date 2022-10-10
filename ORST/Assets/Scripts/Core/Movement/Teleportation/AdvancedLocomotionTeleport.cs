db66f9070a850d6addafe9d7f4c77bfa006088a6
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-10-10 16:23:51 +0200

    Add TeleportedToPoint event that alsop includes the TeleportPoint

--- ORST/Assets/Scripts/Core/Movement/Teleportation/AdvancedLocomotionTeleport.cs
+++ ORST/Assets/Scripts/Core/Movement/Teleportation/AdvancedLocomotionTeleport.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 
 namespace ORST.Core.Movement {
     public class AdvancedLocomotionTeleport : LocomotionTeleport {
@@ -7,6 +8,23 @@ namespace ORST.Core.Movement {
 
         public event Action EnteredIntersection;
         public event Action ExitedIntersection;
+        public event Action<TeleportPoint> TeleportedToPoint;
+
+        /// <summary>
+        /// Start the state machine coroutines.
+        /// </summary>
+        public override void OnEnable() {
+            base.OnEnable();
+
+            Teleported -= OnTeleported;
+            Teleported += OnTeleported;
+        }
+
+        public override void OnDisable() {
+            base.OnDisable();
+
+            Teleported -= OnTeleported;
+        }
 
         public void InvokeOnIntersectEnter() {
             EnteredIntersection?.Invoke();
@@ -14,6 +32,19 @@ namespace ORST.Core.Movement {
 
         public void InvokeOnIntersectExit() {
             ExitedIntersection?.Invoke();
+        }
+
+        private void OnTeleported(Transform controllerTransform, Vector3 position, Quaternion rotation) {
+            if (TargetHandler is not AdvancedTeleportTargetHandlerNode nodeHandler) {
+                return;
+            }
+
+            if (nodeHandler.AimData.TargetHitInfo.collider == null ||
+                nodeHandler.AimData.TargetHitInfo.collider.GetComponent<TeleportPoint>() is not {} teleportPoint) {
+                return;
+            }
+
+            TeleportedToPoint?.Invoke(teleportPoint);
         }
     }
 }
