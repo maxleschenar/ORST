feaff15681d336b76fbb4c06c7b3696525e07a59
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-10-10 13:42:29 +0200

    Optimize `OrNull` extension method when T is not a UnityEngine.Object

--- ORST/Assets/Scripts/Foundation/Extensions/ObjectExtensions.cs
+++ ORST/Assets/Scripts/Foundation/Extensions/ObjectExtensions.cs
@@ -3,7 +3,11 @@
 namespace ORST.Foundation.Extensions {
     public static class ObjectExtensions {
         public static T OrNull<T>(this T obj) where T : class {
-            return obj == null ? null : obj;
+            // ReSharper disable once MergeConditionalExpression, ConditionIsAlwaysTrueOrFalse
+            // Convert Unity fake-null to real null
+            if (obj is Object) return obj == null ? null : obj;
+
+            return obj;
         }
     }
 }
