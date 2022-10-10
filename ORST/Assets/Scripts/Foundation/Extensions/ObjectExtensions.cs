193954e4e78e58f3c3ac8636444fcefb31b37f8c
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-10-10 12:28:49 +0200

    Add ObjectExtensions class with IsNull extension
    OrNull converts a Unity fake-null into a real null which can then be
    safely used with null coalescing operators.

---
+++ ORST/Assets/Scripts/Foundation/Extensions/ObjectExtensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace ORST.Foundation.Extensions {
+    public static class ObjectExtensions {
+        public static T OrNull<T>(this T obj) where T : class {
+            return obj == null ? null : obj;
+        }
+    }
+}
