d08dbd103081311e34ce19e09845b6e8a5b29f87
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-10-11 14:09:05 +0200

    Expose Nodes in Dialogue

--- ORST/Assets/Scripts/Core/Dialogues/Scriptable/Dialogue.cs
+++ ORST/Assets/Scripts/Core/Dialogues/Scriptable/Dialogue.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -6,5 +7,7 @@ namespace ORST.Core.Dialogues {
     [CreateAssetMenu(fileName = "New Dialogue", menuName = "ORST/Dialogues/Dialogue", order = 0)]
     public class Dialogue : SerializedScriptableObject {
         [SerializeField, Required] private List<DialogueNode> m_Nodes = new();
+
+        public ReadOnlyCollection<DialogueNode> Nodes => m_Nodes.AsReadOnly();
     }
 }
