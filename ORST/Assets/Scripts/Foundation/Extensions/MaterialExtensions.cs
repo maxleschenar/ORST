7cde313e7384200a6c04a676718bea35e6818f28
Maurice Johannssen <60900273+MauriceJohannssen@users.noreply.github.com>
2022-10-11 11:31:27 +0200

    Rename material extension class

--- ORST/Assets/Scripts/Foundation/Extensions/MaterialExtensions.cs
+++ ORST/Assets/Scripts/Foundation/Extensions/MaterialExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 namespace ORST.Foundation.Foundation.Extensions {
-    public static class ColorExtensions {
+    public static class MaterialExtensions {
         public static void SetMaterialAlpha(this Material material, float a) {
             Color currentColor = material.color;
             currentColor.a = a;
