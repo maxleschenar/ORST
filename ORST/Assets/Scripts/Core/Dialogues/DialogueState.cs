f292d5b92c94d754a4b45d8b568049738264eaad
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-10-26 14:18:51 +0200

    Cleanup DialogueState

--- ORST/Assets/Scripts/Core/Dialogues/DialogueState.cs
+++ ORST/Assets/Scripts/Core/Dialogues/DialogueState.cs
@@ -1,21 +1,18 @@
 ﻿using System;
 using System.Linq;
 using ORST.Foundation.Extensions;
-using Sirenix.OdinInspector;
 
 namespace ORST.Core.Dialogues {
     [Serializable]
     public struct DialogueState {
-        [ShowInInspector] private readonly Dialogue m_Dialogue;
-        [ShowInInspector] private int m_CurrentNodeIndex;
+        private readonly Dialogue m_Dialogue;
+        private int m_CurrentNodeIndex;
 
-        [ShowInInspector, ShowIf("@" + nameof(Started) + " && !" + nameof(Finished))]
         public DialogueNode CurrentNode => m_Dialogue.OrNull()?.Nodes.ElementAtOrDefault(m_CurrentNodeIndex);
-
-        [ShowInInspector, ReadOnly] public bool Started => m_CurrentNodeIndex >= 0;
-        [ShowInInspector, ReadOnly] public bool Finished => m_CurrentNodeIndex >= (m_Dialogue.OrNull()?.Nodes?.Count ?? -1);
-        [ShowInInspector, ReadOnly] public int CurrentNodeIndex => m_CurrentNodeIndex;
-        [ShowInInspector, ReadOnly] public int NodeCount => m_Dialogue.OrNull()?.Nodes?.Count ?? -1;
+        public bool Started => m_CurrentNodeIndex >= 0;
+        public bool Finished => m_CurrentNodeIndex >= (m_Dialogue.OrNull()?.Nodes?.Count ?? -1);
+        public int CurrentNodeIndex => m_CurrentNodeIndex;
+        public int NodeCount => m_Dialogue.OrNull()?.Nodes?.Count ?? -1;
 
         public DialogueState(Dialogue dialogue) {
             m_Dialogue = dialogue;
