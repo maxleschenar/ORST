cb918049fc150b8e77edb304cd947a4d3f4e2828
Maurice Johannssen <60900273+MauriceJohannssen@users.noreply.github.com>
2022-10-27 13:43:09 +0200

    Extend functionality in door zones

--- ORST/Assets/Scripts/Core/Interactions/DoorZone/DoorZone.cs
+++ ORST/Assets/Scripts/Core/Interactions/DoorZone/DoorZone.cs
@@ -1,14 +1,51 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using Oculus.Interaction;
+using ORST.Core.ModuleTasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace ORST.Core.Interactions
 {
-    public class DoorZone : MonoBehaviour
-    {
+    public class DoorZone : MonoBehaviour {
+        private bool m_DoorsUnlocked;
+        private Grabbable m_DoorHandle;
+        private GameObject m_Player;
+
+        private void Start() {
+            m_DoorHandle = GetComponent<Grabbable>();
+        }
+
         private void OnTriggerEnter(Collider other) {
-            throw new NotImplementedException();
+            if (other.CompareTag("Player")) {
+                m_Player = other.gameObject;
+                List<ModuleTask> remModuleTasks = ModuleTasksManager.Instance.GetRemainingTasks();
+                m_DoorsUnlocked = remModuleTasks.Count <= 0;
+                if (!m_DoorsUnlocked) {
+                    PopupManager.Instance.DisplayTasks(remModuleTasks);
+                    PopupManager.Instance.OpenPopup();
+                }
+                
+                DoorHandleMoved();
+            }
+        }
+
+        private void OnTriggerExit(Collider other) {
+            if (m_Player == other.gameObject) {
+                PopupManager.Instance.ClosePopup();
+            }
+        }
+
+        public void DoorHandleMoved() {
+            Debug.LogWarning("Teleport to next room");
+            StartCoroutine(ChangeScene());
+        }
+
+        private IEnumerator ChangeScene() {
+            yield return new WaitForSeconds(15);
+            OVRScreenFade.instance.FadeOut();
+            yield return new WaitUntil(() => OVRScreenFade.instance.currentAlpha >= 1.0f);
+            SceneManager.LoadScene(10);
         }
     }
 }
