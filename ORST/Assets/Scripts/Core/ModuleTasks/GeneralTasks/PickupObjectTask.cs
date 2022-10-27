20b847cc615d70a4170654625f335941fcb583f4
Maurice Johannssen <60900273+MauriceJohannssen@users.noreply.github.com>
2022-10-27 13:44:04 +0200

    Create 'PickupObject' task

---
+++ ORST/Assets/Scripts/Core/ModuleTasks/GeneralTasks/PickupObjectTask.cs
@@ -0,0 +1,32 @@
+using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
+using UnityEngine;
+using ORST.Core.ModuleTasks;
+
+namespace ORST.Core
+{
+    public class PickupObjectTask : ModuleTask {
+        [SerializeField] private GameObject m_ObjectToPick;
+        private bool m_ObjectPickedUp;
+
+        public override ModuleTaskState ExecuteModuleTask() {
+            return m_ObjectPickedUp ? ModuleTaskState.Successful : ModuleTaskState.Running;
+        }
+
+        private void Start() {
+            HandGrabInteractable interactable = m_ObjectToPick.GetComponentInChildren<HandGrabInteractable>();
+            if (interactable != null) {
+                interactable.WhenPointerEventRaised += ProcessPointerEvent;
+                return;
+            }
+
+            Debug.Log("Task::Object in 'PickupObjectTask' does not contain 'HandGrabInteractable' component.");
+        }
+
+        private void ProcessPointerEvent(PointerEvent pointerEvent) {
+            if (pointerEvent.Type == PointerEventType.Select) {
+                m_ObjectPickedUp = true;
+            }
+        }
+    }
+}
