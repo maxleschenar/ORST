d3da4a491d175231046c3ad181393c4fe175c237
Maurice Johannssen <60900273+MauriceJohannssen@users.noreply.github.com>
2022-10-27 14:08:30 +0200

    Add teleport task

---
+++ ORST/Assets/Scripts/Core/ModuleTasks/GeneralTasks/GoToTeleportPointTask.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ORST.Core.ModuleTasks;
+using Oculus.Interaction;
+
+namespace ORST.Core
+{
+    public class GoToTeleportPointTask : ModuleTask {
+        [SerializeField] private LocomotionTeleport m_LocomotionTeleport;
+        
+        private void Start() {
+            m_LocomotionTeleport.EnterStateTeleporting += ProcessTeleportingEvent;
+        }
+
+        private void ProcessTeleportingEvent() {
+            //m_LocomotionTeleport.
+        }
+        
+        public override ModuleTaskState ExecuteModuleTask() {
+            //LocomotionTeleport.
+            return ModuleTaskState.Running;
+        }
+    }
+}
