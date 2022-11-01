796673027c135546a640c2c5d6f53beb24dd48c3
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-11-01 11:03:36 +0100

    Add support for tracking GoToTeleportPointTask only after starting

--- ORST/Assets/Scripts/Core/ModuleTasks/GeneralTasks/GoToTeleportPointTask.cs
+++ ORST/Assets/Scripts/Core/ModuleTasks/GeneralTasks/GoToTeleportPointTask.cs
@@ -1,24 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using ORST.Core.ModuleTasks;
-using Oculus.Interaction;
+using ORST.Core.Movement;
+using Sirenix.OdinInspector;
 
 namespace ORST.Core {
     public class GoToTeleportPointTask : ModuleTask {
-        [SerializeField] private LocomotionTeleport m_LocomotionTeleport;
-        
-        private void Start() {
-            m_LocomotionTeleport.EnterStateTeleporting += ProcessTeleportingEvent;
+        [SerializeField, Required] private AdvancedLocomotionTeleport m_LocomotionTeleport;
+        [SerializeField, Required] private TeleportPointORST m_TeleportPoint;
+        [LabelText("[?] Track Only When Running"), Tooltip("If this is true then the task will only update while the task is running.")]
+        [SerializeField] private bool m_TrackOnlyWhenRunning = true;
+
+        private bool m_Teleported;
+
+        private void OnEnable() {
+            m_LocomotionTeleport.TeleportedToPoint += OnTeleportedToPoint;
         }
 
-        private void ProcessTeleportingEvent() {
-            //m_LocomotionTeleport.
+        private void OnDisable() {
+            m_LocomotionTeleport.TeleportedToPoint -= OnTeleportedToPoint;
         }
-        
+
+        private void OnTeleportedToPoint(TeleportPointORST teleportPoint) {
+            if ((!m_TrackOnlyWhenRunning || Started) && teleportPoint == m_TeleportPoint) {
+                m_Teleported = true;
+            }
+        }
+
+        protected override void OnModuleTaskStarted() {
+            if (m_TrackOnlyWhenRunning) {
+                m_Teleported = false;
+            }
+        }
+
         public override ModuleTaskState ExecuteModuleTask() {
-            //LocomotionTeleport.
-            return ModuleTaskState.Running;
+            return m_Teleported ? ModuleTaskState.Successful : ModuleTaskState.Running;
         }
     }
 }
