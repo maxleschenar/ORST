df0e3a9f6c96d5529c5d32f60770cf62afe41c4e
Maurice Johannssen <60900273+MauriceJohannssen@users.noreply.github.com>
2022-11-01 13:29:31 +0100

    Implement door zone and prefab

--- ORST/Assets/Scripts/Core/Interactions/DoorZone/DoorZone.cs
+++ ORST/Assets/Scripts/Core/Interactions/DoorZone/DoorZone.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
 using ORST.Core.ModuleTasks;
 using ORST.Core.UI;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace ORST.Core.Interactions {
     public class DoorZone : MonoBehaviour {
+        [SerializeField, Required] private HandGrabInteractable m_DoorHandle;
+        private OneGrabRotateTransformer m_DoorHandleRotateTransformer;
         private bool m_DoorsUnlocked;
-        private Grabbable m_DoorHandle;
+        private bool m_TransitionStarted;
         private GameObject m_Player;
 
         private void Start() {
-            m_DoorHandle = GetComponent<Grabbable>();
+            if (m_DoorHandle != null) {
+                m_DoorHandle.WhenPointerEventRaised += ProcessPointerEvent;
+                m_DoorHandleRotateTransformer = m_DoorHandle.transform.GetComponent<OneGrabRotateTransformer>();
+            }
+        }
+
+        private void OnDestroy() {
+            if (m_DoorHandle != null) {
+                m_DoorHandle.WhenPointerEventRaised -= ProcessPointerEvent;
+            }
         }
 
         private void OnTriggerEnter(Collider other) {
@@ -28,8 +41,6 @@ namespace ORST.Core.Interactions {
                 PopupManager.Instance.DisplayTasks(remainingTasks);
                 PopupManager.Instance.OpenPopup();
             }
-
-            DoorHandleMoved();
         }
 
         private void OnTriggerExit(Collider other) {
@@ -44,10 +55,24 @@ namespace ORST.Core.Interactions {
         }
 
         private IEnumerator ChangeScene() {
-            yield return new WaitForSeconds(15);
             OVRScreenFade.instance.FadeOut();
             yield return new WaitUntil(() => OVRScreenFade.instance.currentAlpha >= 1.0f);
             SceneManager.LoadScene(10);
+        }
+
+        private void ProcessPointerEvent(PointerEvent pointerEvent) {
+            if (pointerEvent.Type != PointerEventType.Move || m_TransitionStarted) {
+                return;
+            }
+
+            if (m_DoorHandleRotateTransformer.Constraints.MaxAngle.Constrain &&
+                !Mathf.Approximately(m_DoorHandle.transform.rotation.eulerAngles.z,
+                                     m_DoorHandleRotateTransformer.Constraints.MaxAngle.Value)) {
+                return;
+            }
+
+            StartCoroutine(ChangeScene());
+            m_TransitionStarted = true;
         }
     }
 }
