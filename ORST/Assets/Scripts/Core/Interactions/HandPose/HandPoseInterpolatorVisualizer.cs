e3a546af304f994f18fb6b4513a2166a66f96448
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-11-08 13:46:34 +0100

    Add HandPoseInterpolatorVisualizer script

---
+++ ORST/Assets/Scripts/Core/Interactions/HandPose/HandPoseInterpolatorVisualizer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Oculus.Interaction.HandGrab;
+using Oculus.Interaction.HandGrab.Visuals;
+using ORST.Foundation.Core;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ORST.Core.Interactions {
+    [ExecuteAlways]
+    public class HandPoseInterpolatorVisualizer : BaseMonoBehaviour {
+        [SerializeField, Required] private HandPoseData m_HandPoseA;
+        [SerializeField, Required] private HandPoseData m_HandPoseB;
+        [SerializeField, Required] private HandPuppet m_Puppet;
+        [Space]
+        [SerializeField, Range(0.0f, 1.0f), OnValueChanged(nameof(OnTChanged))] private float m_T;
+
+        private HandPose m_InterpolatedPose;
+
+        private void Update() {
+            Interpolate();
+        }
+
+        private void Interpolate() {
+            HandPoseInterpolator.Interpolate(m_HandPoseA, m_HandPoseB, m_InterpolatedPose ??= new HandPose(m_HandPoseA.HandPose), m_T);
+            m_Puppet.SetJointRotations(m_InterpolatedPose.JointRotations);
+        }
+
+        private void OnTChanged() {
+            if (m_HandPoseA == null || m_HandPoseB == null || m_Puppet == null) {
+                return;
+            }
+
+            Interpolate();
+        }
+    }
+}
